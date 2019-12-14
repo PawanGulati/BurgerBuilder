fbc58f0ae7633e8f8b81c12a9cf7a5f2f988332d
ProDaddy <undefended0@gmail.com>
2019-12-14 16:17:42 +0530

    Added Burger component in this statefull component

---
+++ src/components/BurgerBuilder/burgerBuilder.components.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Burger from '../../components/Burger/Burger.components'
+
+export default props => 
+    <div>
+        <Burger/>
+        burger-controls
+    </div>
