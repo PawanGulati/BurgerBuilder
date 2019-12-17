2bd496885da668966c5c6355713b650f5b45b011
ProDaddy <undefended0@gmail.com>
2019-12-17 13:30:45 +0530

    Gave dynamic rendring to burgerIngrdients

--- src/components/Burger/Burger.components.jsx
+++ src/components/Burger/Burger.components.jsx
@@ -3,14 +3,27 @@ import React  from 'react'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients.components'
 import classes from './Burger.module.css'
 
-console.log(classes)
+export default ({ingredients}) =>{
+    let ArrayIngredients = Object.keys(ingredients).map(ing =>{
+        return [...Array(ingredients[ing])].map((_ , i) => {
+            return <BurgerIngredients type={ing} key={ing+i} />
+        })
+    }).reduce((arr,el)=>{
+        return arr.concat(el)
+    },[])
 
-export default props =>
-    <div className={classes.Burger}>
-        <BurgerIngredients type='bread-top' />
-        <BurgerIngredients type='cheese' />
-        <BurgerIngredients type='bacon' />
-        <BurgerIngredients type='cheese' />
-        <BurgerIngredients type='bread-bottom' />
-    </div>
+    if(ArrayIngredients.length === 0){
+        ArrayIngredients = <p>Please start adding somethin' </p>
+    }     
+
+    return(
+        <div className={classes.Burger}>
+            <BurgerIngredients type='bread-top' />
+            {ArrayIngredients} 
+            <BurgerIngredients type='bread-bottom' />
+        </div> 
+    )    
+
+}
+
     
