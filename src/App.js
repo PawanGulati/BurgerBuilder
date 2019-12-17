4ea14866467097bbd1e03bf3b3695054c925b7f3
ProDaddy <undefended0@gmail.com>
2019-12-17 13:27:21 +0530

    Added BurgerBuilder Component to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Layout from './components/Layouts/layout'
-import BurgerBuilder from './components/BurgerBuilder/burgerBuilder.components'
+import BurgerBuilder from './containers/BurgerBuilder/burgerBuilder.components'
 
 function App() {
   return (
