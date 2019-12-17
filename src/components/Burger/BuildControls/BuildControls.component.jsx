0ce550f2216714299a57cd7841f942d838aa826b
ProDaddy <undefended0@gmail.com>
2019-12-17 20:06:28 +0530

    Added orderNow button and its css

---
+++ src/components/Burger/BuildControls/BuildControls.component.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+import ButtonControl from './BuildControl/BuildControl.component'
+
+import classes from './BuildControls.module.css'
+
+export default ({ingredients,AddIngr,RemoveIngr,price,purchasable}) =>{
+    return <div className={classes.BuildControls}>
+        <p>Current Price :: <strong>{price}</strong></p>
+        {Object.keys(ingredients).map((ing,i) => {
+            return <ButtonControl AddIngr={AddIngr} RemoveIngr={RemoveIngr} key={ing+i} label={ing.toUpperCase()} type={ing}/>
+        })}
+        <button className={classes.OrderButton} disabled = {!purchasable} >ORDER NOW</button>
+    </div>
+} 
