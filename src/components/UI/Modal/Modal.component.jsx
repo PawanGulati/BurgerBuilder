46d1426d5ef05a76d4df18ea1e51397ff2f83f78
ProDaddy <undefended0@gmail.com>
2019-12-18 12:06:03 +0530

    Added Backdrop to modal

--- src/components/UI/Modal/Modal.component.jsx
+++ src/components/UI/Modal/Modal.component.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 import classes from './Modal.module.css'
 
-export default props => 
-    <div className={classes.Modal}>
-        {props.children}
-    </div>
+import BackDrop from '../BackDrop/BackDrop'
+import Aux from '../../../hoc/Auxillary'
+
+export default props =>
+    <Aux>
+        <BackDrop show={props.show} clicked={props.backDropClicked}/>
+        <div className={classes.Modal} style={{
+            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: props.show ? '1' : '0'
+        }}>
+            {props.children}
+        </div>
+    </Aux>
