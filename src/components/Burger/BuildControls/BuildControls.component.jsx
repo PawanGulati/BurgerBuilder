4428d065872b03f1bc4805625e4a7d341f2ccfd5
ProDaddy <undefended0@gmail.com>
2019-12-18 12:04:06 +0530

    Order Now button gets functionality to open modal

--- src/components/Burger/BuildControls/BuildControls.component.jsx
+++ src/components/Burger/BuildControls/BuildControls.component.jsx
@@ -4,12 +4,13 @@ import ButtonControl from './BuildControl/BuildControl.component'
 
 import classes from './BuildControls.module.css'
 
-export default ({ingredients,AddIngr,RemoveIngr,price,purchasable}) =>{
+export default ({ingredients,AddIngr,RemoveIngr,price,purchasable,orderIt}) =>{
+    
     return <div className={classes.BuildControls}>
         <p>Current Price :: <strong>{price}</strong></p>
         {Object.keys(ingredients).map((ing,i) => {
             return <ButtonControl AddIngr={AddIngr} RemoveIngr={RemoveIngr} key={ing+i} label={ing.toUpperCase()} type={ing}/>
         })}
-        <button className={classes.OrderButton} disabled = {!purchasable} >ORDER NOW</button>
+        <button className={classes.OrderButton} disabled = {!purchasable} onClick={orderIt}>ORDER NOW</button>
     </div>
 } 
