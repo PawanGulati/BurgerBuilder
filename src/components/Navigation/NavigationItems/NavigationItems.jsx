c8c37486c246b4c9c5e74910632ccc94c4a07e3e
ProDaddy <undefended0@gmail.com>
2019-12-19 05:06:50 +0530

    Created NavItems component with NavItem n styling

---
+++ src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import NavItem from '../NavigationItem/NavigationItem'
+import classes from './NavigationItems.module.css'
+
+export default props => 
+        <ul className={classes.NavItems}>
+            <NavItem>Home</NavItem>
+        </ul>
