e031bb3204775a497d4ef631aa994fcae86e0f59
ProDaddy <undefended0@gmail.com>
2019-12-19 07:23:06 +0530

    Added SideDrawerToggle to ToolBar

--- src/components/Navigation/ToolBar/Toolbar.jsx
+++ src/components/Navigation/ToolBar/Toolbar.jsx
@@ -3,10 +3,13 @@ import classes from './ToolBar.module.css'
 
 import Logo from '../../Logo/Logo'
 import NavItems from '../NavigationItems/NavigationItems'
+import SideDrawerToggle from '../SideDrawer/SideDrawerToggle/SideDrawerToggle'
 
 export default props => 
     <header className={classes.ToolBar}>
-        <div style={{flex:1}}>Menu</div>
+        <div style={{flex:1}} className={classes.MobileOnly}>
+            <SideDrawerToggle clicked={props.clicked}/>
+        </div>
         <Logo hgt={'90%'}/>
         <nav className={classes.DesktopOnly}>
             <NavItems/>
