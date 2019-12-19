e7b3205234ae3856822c6ecc6ed491d76806b30e
ProDaddy <undefended0@gmail.com>
2019-12-19 06:36:14 +0530

    Responsive Adjustment

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,6 +4,6 @@ import BurgerLogo from '../../assets/images/burger-logo.png'
 import classes from './Logo.module.css'
 
 export default props => 
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height:props.hgt}}>
         <img src={BurgerLogo} alt=' Logo '/>
     </div>
--- src/components/Navigation/ToolBar/Toolbar.jsx
+++ src/components/Navigation/ToolBar/Toolbar.jsx
@@ -7,8 +7,8 @@ import NavItems from '../NavigationItems/NavigationItems'
 export default props => 
     <header className={classes.ToolBar}>
         <div style={{flex:1}}>Menu</div>
-        <Logo/>
-        <nav>
+        <Logo hgt={'90%'}/>
+        <nav className={classes.DesktopOnly}>
             <NavItems/>
         </nav>
     </header>
