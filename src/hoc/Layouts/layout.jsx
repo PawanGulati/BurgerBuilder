1c353a36f88aba1a8380a7ce9a0c30075e648582
ProDaddy <undefended0@gmail.com>
2019-12-19 06:37:39 +0530

    Made Layout RCC n Added sidedrawer functionalities

--- src/hoc/Layouts/layout.jsx
+++ src/hoc/Layouts/layout.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../hoc/Auxillary'
 import Toolbar from '../../components/Navigation/ToolBar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 
-export default ({children}) =>
-    <Aux>
-        <Toolbar/> 
-        <main>
-            {children}
-        </main>
-    </Aux>
+export default class extends Component{
+    
+    state={
+        sideDrawer:true
+    }
+
+    ToggleSideDrawer = () =>{
+        this.setState({
+            sideDrawer : !this.state.sideDrawer
+        })
+    }
+
+    render(){
+        return(
+            <Aux>
+                <SideDrawer clicked={this.ToggleSideDrawer} show={this.state.sideDrawer}/>
+                <Toolbar/> 
+                <main>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+} 
     
