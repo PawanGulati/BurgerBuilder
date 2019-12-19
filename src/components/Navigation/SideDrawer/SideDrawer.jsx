4d230cb3dc23cbe8b82db511f0aece4746596248
ProDaddy <undefended0@gmail.com>
2019-12-19 06:33:32 +0530

    Implemented SideDrawer and its styling

---
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './SideDrawer.module.css'
+
+import Logo from '../../Logo/Logo'
+import NavItems from '../NavigationItems/NavigationItems'
+import BackDrop from '../../UI/BackDrop/BackDrop'
+
+export default props =>{
+    return <> 
+        <BackDrop clicked={props.clicked} show={props.show}/>
+        <div className={`${classes.SideDrawer} ${props.show ? classes.Open : classes.Close}`}>
+            <Logo hgt={'11%'}/>
+            <nav>
+                <NavItems/>
+            </nav>
+        </div>
+    </>
+} 
