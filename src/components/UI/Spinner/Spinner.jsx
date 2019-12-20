8b7c45779c84499fbbfc5bc64bd112c3d7508c10
ProDaddy <undefended0@gmail.com>
2019-12-20 11:43:21 +0530

    Modified Spinner

--- src/components/UI/Spinner/Spinner.jsx
+++ src/components/UI/Spinner/Spinner.jsx
@@ -2,4 +2,10 @@ import React from 'react'
 import classes from './Spinner.module.css'
 
 export default props => 
-    <div className={classes.Loader}>Loading...</div>
+    // <div className={classes.Loader}>Loading...</div>
+    //modified spinner LOL
+    <div className={classes.fancySpinner}>
+        <div className={classes.ring}></div>
+        <div className={classes.ring}></div>
+        <div className={classes.dot}></div>
+    </div>
