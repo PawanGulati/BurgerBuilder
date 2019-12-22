a03b9b89e788876e8384316f89b67081feee3876
ProDaddy <undefended0@gmail.com>
2019-12-22 13:21:29 +0530

    Added routing for '/checkout ' n '/ '

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 
 import Layout from './hoc/Layouts/layout'
 import BurgerBuilder from './containers/BurgerBuilder/burgerBuilder.components'
+import Checkout from './containers/Checkout/Checkout';
+
+import {Route,Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <BurgerBuilder/>  
+
+        <Switch>
+          <Route path='/checkout' component={Checkout}/>
+          <Route path='/' component={BurgerBuilder}/>        
+        </Switch>
       </Layout>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-const app = <BrowserRouter> <App/> </BrowserRouter>
+const app = (<BrowserRouter> <App/> </BrowserRouter>)
 
 render(app, document.getElementById('root'));
 
