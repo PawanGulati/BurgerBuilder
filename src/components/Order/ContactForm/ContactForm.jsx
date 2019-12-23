09168c30c71bb7797bf2a801593c686e886fd342
ProDaddy <undefended0@gmail.com>
2019-12-23 13:32:30 +0530

    Dynamic rendrin' ContactForm component n styled it

---
+++ src/components/Order/ContactForm/ContactForm.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Input from '../../UI/Input/Input'
+import classes from './ContactForm.module.css'
+import Button from '../../UI/Button/Button'
+
+
+export default props => {
+    const formElemArr=[]
+    for(let key in props.orderForm){
+        formElemArr.push({
+            id:key,
+            config:props.orderForm[key]
+        })
+    }
+
+    console.log(formElemArr);
+    
+
+    return( 
+        <div className={classes.Form}>
+            <h4> Enter Your Details Here </h4>
+            <form >
+                {formElemArr.map(formEl => (
+                    <div key={formEl.id} style={{margin:'2px'}}>
+                        <label style={{margin:'6px',fontWeight:'bold'}}>{formEl.id}</label>
+                        <Input ElemType={formEl.config.ElemType}
+                            ElemConfig={formEl.config.ElemConfig}
+                            value={formEl.config.value}
+                            key={formEl.id}/> 
+                    </div>
+                    ))
+                }    
+            </form>
+            <div>
+                <Button btnType='Danger'clicked={props.cancelOrder}> CANCEL </Button>
+                <Button btnType='Success'clicked={props.continueOrder}>!! ORDER IT !! </Button>
+            </div>
+
+        </div>
+    )
+}
