e44187fd33c988c1b377989f43710bc8c477dcc2
ProDaddy <undefended0@gmail.com>
2019-12-27 16:58:00 +0530

    Added REDUX devtool and THUNK as MIDDLEWARE

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 //Starting w/ REDUX
-import {createStore} from 'redux'
+import {createStore,applyMiddleware,compose} from 'redux'
 import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 
 import rootReducer from './store/reducers/reducers'
 
-const store = createStore(rootReducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)))
 
 const app = (
     <Provider store={store}>
